40b05d5d850ac293c2a8bc280f17242246517e64
Crowdbotics <team@crowdbotics.com>
2022-02-25 17:08:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cleaning_maid_easy__33849",
+  url: "https://cleaning_maid_easy__33849.botics.co",
+  api: "https://cleaning_maid_easy__33849.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
