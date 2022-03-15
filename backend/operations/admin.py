2526bdb30c1a11e2f52879c28f8c0469b485f3fc
Omar Dalegah <omar.dalegah@gmai.com>
2022-03-15 20:01:19 +0200

    Admin register models

--- backend/operations/admin.py
+++ backend/operations/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import (
+    Note,
+    Team,
+    Frequency,
+    Service,
+    Appointment
+)
 # Register your models here.
+
+admin.site.register(Note)
+admin.site.register(Team)
+admin.site.register(Frequency)
+admin.site.register(Service)
+admin.site.register(Appointment)
