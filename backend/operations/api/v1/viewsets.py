4882c136ec258e9ca800150b0147f721738a67c2
Omer Delice <omar.dalegah@gmail.com>
2022-05-25 19:42:22 +0200

    date fix

--- backend/operations/api/v1/viewsets.py
+++ backend/operations/api/v1/viewsets.py
@@ -201,7 +201,7 @@ class AddTeamMemberViewSet(ViewSet):
         if day_off:
             for member in members:
                 member_membership = TeamMembershipRecord.objects.filter(
-                    record_date=day_off,
+                    record_date__date=day_off,
                     member=member,
                 )
                 if len(member_membership) > 0:
